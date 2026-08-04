4bf64e873ca63321cc9bfbcce95c9f2a75e52582
KLeber Erbes <klebererbes70@gmail.com>
2026-08-03 23:38:46 -0300

    .

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const CONFIG = {
   supabaseKey: 'sb_publishable_XmVYyFWVyaG3zilJg6Otpg_YNYTWAGG',
   appsScriptUrl: 'https://script.google.com/macros/s/AKfycbzk9p47SYi4t9HEotN6FmelyTwf3nuioTsDDbR2TdqvTX7NDldxmev7VxTgQpLS5A1E/exec',
   whatsappNumber: '554733752227',
-  horario: { pedidos: { h: 8, m: 0 }, abertura: { h: 14, m: 0 }, fechamento: { h: 14, m: 0 } },
+  horario: { pedidos: { h: 8, m: 0 }, abertura: { h: 24, m: 0 }, fechamento: { h: 24, m: 0 } },
   cartExpireHours: 4,
   limits: { acompMax: 6, carneMax: 3, saladaMax: 3 }
 };
